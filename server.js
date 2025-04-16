3c42ba75ee7d6161cc506fe3f293d45a20efccf8
Anujith Nayak <anujithsnayak@gmail.com>
2025-04-16 17:28:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // allows your website to call this API
+
+// Update these details with your actual database info
+const db = mysql.createConnection({
+  host: 'sql12.freesqldatabase.com',
+  user: 'sql12773414',
+  password: 'aHiAB9Krh9',
+  database: 'sql12773414'
+});
+
+app.get('/songs', (req, res) => {
+  db.query('SELECT * FROM SONGS', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
